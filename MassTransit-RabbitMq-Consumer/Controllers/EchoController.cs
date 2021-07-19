860b4178aac0c3927b2d1a043273914462025ed1
Wender Dalber <wenderdalber@gmail.com>
2021-07-18 21:36:50 -0300

    start the project, consumer the queue

---
+++ MassTransit-RabbitMq-Consumer/Controllers/EchoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MassTransit_RabbitMq_Consumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Post()
+        {
+            return Ok("Ok");
+        }
+    }
+}
---
+++ MassTransit-RabbitMq-Consumer/ProductConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using MassTransit_RabbitMq_Producer.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MassTransit_RabbitMq_Consumer
+{
+    public class ProductConsumer : IConsumer<Product>
+    {
+        private readonly ILogger<ProductConsumer> _logger;
+
+        public ProductConsumer(ILogger<ProductConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<Product> context)
+        {
+            try
+            {
+                var product = context.Message;
+                product.DateUpdatePrice = DateTime.Now;
+                _logger.LogInformation($"Product received: {product.Id}");
+            }
+            catch (Exception ex) { _logger.LogError("Error on try to consume product", ex); }
+            return Task.CompletedTask;
+        }
+    }
+}
